5ee7135a5fc6986c0b001008d959dcc106442a15
wufanwillan <wufanwillan@gmail.com>
2017-10-06 22:45:18 +0800

    Update Intersection of Two Arrays II.py

--- Intersection of Two Arrays II.py
+++ Intersection of Two Arrays II.py
@@ -29,3 +29,15 @@ class Solution(object):
         for key,time in comb.items():
             result.extend([key]*time)
         return result
+    
+   def intersect2(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        c = collections.Counter(nums1) & collections.Counter(nums2)
+        intersect = []
+        for i in c:
+            intersect.extend([i] * c[i])
+        return intersect
