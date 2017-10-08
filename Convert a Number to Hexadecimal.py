be77bf1d42f13aa9c5975a2efc3332d6aad0a5e1
wufanwillan <wufanwillan@gmail.com>
2017-10-08 19:07:55 +0800

    Update Convert a Number to Hexadecimal.py

--- Convert a Number to Hexadecimal.py
+++ Convert a Number to Hexadecimal.py
@@ -44,3 +44,24 @@ class Solution(object):
         result.reverse()
 
         return ''.join(result)
+
+    class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        if not num:
+            return "0"
+
+        result = []
+        while num and len(result) != 8:
+            h = num & 15
+            if h < 10:
+                result.append(str(chr(ord('0') + h)))
+            else:
+                result.append(str(chr(ord('a') + h-10)))
+            num >>= 4
+        result.reverse()
+
+        return "".join(result)
