7a32acbf6c1f0c3c62ee74df8077d48137f28836
wufanwillan <wufanwillan@gmail.com>
2017-10-09 17:28:02 +0800

    Update Climbing Stairs.py

--- Climbing Stairs.py
+++ Climbing Stairs.py
@@ -28,3 +28,27 @@ class Solution(object):
             result+=comb(n,count)
             n-=1
             count+=1
+            
+ class Solution:
+    """
+    :type n: int
+    :rtype: int
+    """
+    def climbStairs(self, n):
+        prev, current = 0, 1
+        for i in xrange(n):
+            prev, current = current, prev + current, 
+        return current
+
+    # Time:  O(2^n)
+    # Space: O(n)
+    def climbStairs1(self, n):
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+if __name__ == "__main__":
+    result = Solution().climbStairs(2)
+    print result
