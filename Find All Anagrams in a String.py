e1951ca9d5be782b244cba7dd7d2950fde5dd19f
wufanwillan <wufanwillan@gmail.com>
2018-03-12 00:17:59 +0800

    Create Find All Anagrams in a String.py

---
+++ Find All Anagrams in a String.py
@@ -0,0 +1,55 @@
+# Given a string s and a non-empty string p, find all the start indices of p's anagrams in s.
+
+# Strings consists of lowercase English letters only and the length of both strings s and p will not be larger than 20,100.
+
+# The order of output does not matter.
+
+# Example 1:
+
+# Input:
+# s: "cbaebabacd" p: "abc"
+
+# Output:
+# [0, 6]
+
+# Explanation:
+# The substring with start index = 0 is "cba", which is an anagram of "abc".
+# The substring with start index = 6 is "bac", which is an anagram of "abc".
+# Example 2:
+
+# Input:
+# s: "abab" p: "ab"
+
+# Output:
+# [0, 1, 2]
+
+# Explanation:
+# The substring with start index = 0 is "ab", which is an anagram of "ab".
+# The substring with start index = 1 is "ba", which is an anagram of "ab".
+# The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+class Solution:
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        ls=len(s)
+        lp=len(p)
+        result=[]
+        i=0
+        if lp>ls:
+            return []
+        from collections import Counter
+        deque=Counter(s[:lp])
+        pair=Counter(p)
+        while 1:
+            if deque-pair=={}:
+                result.append(i)
+            i+=1
+            if i+lp>ls:
+                break
+            deque.subtract(s[i-1])
+            deque.update(s[i+lp-1])
+        return result
