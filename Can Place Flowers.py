c4385e4e531b6d1778aa55139ccf4977d7b274f9
wufanwillan <wufanwillan@gmail.com>
2018-03-14 22:09:15 +0800

    Create Can Place Flowers.py

---
+++ Can Place Flowers.py
@@ -0,0 +1,41 @@
+# Suppose you have a long flowerbed in which some of the plots are planted and some are not. However, flowers cannot be planted in adjacent plots - they would compete for water and both would die.
+
+# Given a flowerbed (represented as an array containing 0 and 1, where 0 means empty and 1 means not empty), and a number n, return if n new flowers can be planted in it without violating the no-adjacent-flowers rule.
+
+# Example 1:
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: True
+# Example 2:
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: False
+# Note:
+# The input array won't violate no-adjacent-flowers rule.
+# The input array size is in the range of [1, 20000].
+# n is a non-negative integer which won't exceed the input array size.
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed, n):
+        """
+        :type flowerbed: List[int]
+        :type n: int
+        :rtype: bool
+        """
+        if n==0:
+            return True
+        
+        flower=[0]+flowerbed+[0]
+        ls=len(flower)
+        i,count=1,0
+        while i<ls-1:
+            if not (flower[i-1] or flower[i] or flower[i+1]):
+                flower[i]=1
+                n-=1
+                if n==0:
+                    break
+                i+=2
+            else:
+                i+=1
+        if n==0:
+            return True
+        else:
+            return False
