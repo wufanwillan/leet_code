8a577de714cafe95386d88e5fa00a6a17f377f2b
wufanwillan <wufanwillan@gmail.com>
2018-03-14 21:49:26 +0800

    Create Nth Digit.py

---
+++ Nth Digit.py
@@ -0,0 +1,39 @@
+# Find the nth digit of the infinite integer sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...
+
+# Note:
+# n is positive and will fit within the range of a 32-bit signed integer (n < 231).
+
+# Example 1:
+
+# Input:
+# 3
+
+# Output:
+# 3
+# Example 2:
+
+# Input:
+# 11
+
+# Output:
+# 0
+
+# Explanation:
+# The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+
+class Solution:
+    def findNthDigit(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """        
+        digit_type=1
+        digit_num=9
+        while n>digit_type*digit_num:
+            n-=digit_type*digit_num
+            digit_type+=1
+            digit_num*=10
+        base=(n-1)//digit_type
+        num=str(pow(10,digit_type-1)+base)
+        
+        return int(num[(n-1)%digit_type])
